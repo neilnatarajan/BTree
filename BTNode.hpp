7e7eb002e48f5926e2556cffcb606a5d2430ed85
Neil Natarajan <neilnatarajan@yahoo.com>
2017-03-20 14:54:37 -0700

    README contains B tree def

---
+++ BTNode.hpp
@@ -0,0 +1,16 @@
+#ifndef BTNODE_HPP
+#define BTNODE_HPP
+
+
+//header file for BTNode class aka single node of the B Tree
+
+class BTNode{
+
+private: 
+	int numKeys;		//each node will keep track of the number of keys it contains 
+	
+
+};
+
+
+#endif 
